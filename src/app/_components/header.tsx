963204c76ee01d357cf93b9fbf8af6f586347865
Espiropapas <marissa.calderon03@utrgv.edu>
2025-04-26 15:14:58 -0500

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -5,7 +5,8 @@ const Header = () => {
     <h2 className="text-2xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
       <Link href="/"> 
         <a className="hover:underline">
-        Programming Languages<a/>
+          Programming Languages
+        </a>
       </Link>
       .
     </h2>
