e181e0a1e8ad0e1ba648ad63f8a69f4f23403900
Espiropapas <marissa.calderon03@utrgv.edu>
2025-04-26 15:13:25 -0500

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
-      <Link href="/" className="hover:underline">
-        Programming Languages
+      <Link href="/"> 
+        <a className="hover:underline">
+        Programming Languages<a/>
       </Link>
       .
     </h2>
