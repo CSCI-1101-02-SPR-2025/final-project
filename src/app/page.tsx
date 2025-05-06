31cbfc536d96103d4bd4a9263d1d83e1cbf38594
Espiropapas <marissa.calderon03@utrgv.edu>
2025-05-06 16:20:27 -0500

    Update page.tsx re-order sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import { getAllPosts } from "@/lib/api";
 export default function Index() {
   const allPosts = getAllPosts();
 
-  const heroPost = allPosts[0];
+  const heroPost = allPosts.find(post => post.slug === "dynamic-routing");
 
-  const morePosts = allPosts.slice(1);
+  const morePosts = allPosts.filter(post => post.slug !== "dynamic-routing");
 
   return (
     <main>
